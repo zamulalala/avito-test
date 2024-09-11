5f4c3481c2c26f333384685f0fa1ae13a46b7e7b
zamulalala <ritazamula2001@gmail.com>
2024-09-11 18:31:55 +0300

    Исправлен роутинг для GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdvertisementsPage from './pages/AdvertisementsPage';
 import AdvertisementPage from './pages/AdvertisementPage';
 import OrdersPage from './pages/OrdersPage';
